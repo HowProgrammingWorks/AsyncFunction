ca1300d5f0d3dd018a9b132ab3da693c9c2f65cc
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-02-19 13:41:01 +0200

    Add more async constructor explanations

--- JavaScript/2-constructor.js
+++ JavaScript/2-constructor.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const AsyncFunction = (async () => {}).constructor;
-console.log(AsyncFunction);
+console.log(AsyncFunction); // [Function: AsyncFunction]
 
 const asyncFunctionPrototype = Object.getPrototypeOf(async () => {});
-console.log(asyncFunctionPrototype);
+console.log(asyncFunctionPrototype); // // Function [AsyncFunction] {}
+console.log(AsyncFunction.prototype === asyncFunctionPrototype); // true
 
-console.log(AsyncFunction.prototype === asyncFunctionPrototype);
+const AsyncConstructor = asyncFunctionPrototype.constructor;
+console.log(AsyncConstructor); // [Function: AsyncFunction]
+console.log(AsyncFunction === AsyncConstructor); // true
