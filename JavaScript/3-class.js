a9e1549d7ae9b2dd470c63200f14f4c80fc76c44
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-02-20 13:45:13 +0200

    Add async/await examples

---
+++ JavaScript/3-class.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const Entity = class {
+  constructor(n) {
+    return new Promise((resolve) => {
+      this.n = n;
+      return resolve(this);
+    });
+  }
+  async asyncMethod() {}
+  static async asyncStaticMethod() {}
+};
+
+(async () => {
+  const instance = await new Entity(100);
+  console.log({ instance });
+
+  const { asyncMethod } = instance;
+  console.log({ asyncMethod });
+})();
+
+const { asyncStaticMethod } = Entity;
+console.log({ asyncStaticMethod });
---
+++ JavaScript/4-await.js
@@ -0,0 +1,26 @@
+'use strict';
+
+(async () => {
+
+  // Async function
+
+  const add = async (a, b) => a + b;
+
+  const res1 = await add(2, 3);
+  console.log(`await add(2, 3) = ${res1}`);
+
+  // Promise-returning function
+
+  const sum = (a, b) => new Promise((resolve) => resolve(a + b));
+
+  const res2 = await sum(2, 3);
+  console.log(`await sum(2, 3) = ${res2}`);
+
+  // Promise-returning function with Promise static method
+
+  const total = (a, b) => Promise.resolve(a + b);
+
+  const res3 = await total(2, 3);
+  console.log(`await total(2, 3) = ${res3}`);
+
+})();
